ebc15af5e9f5324fd9c080637bce1eacd6184988
Lukasz Kallas <lukasz.kallas@gmail.com>
2022-10-21 17:07:05 +0200

    Add useMemo & useCallback

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,21 +1,23 @@
 import styles from './Product.module.scss';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductForm from '../ProductForm/ProductForm';
-import { useState } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 
 const Product = (props) => {
 	const [currentColor, setCurrentColor] = useState(props.colors[0]);
 	const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
 
-	const getPrice = () => {
+	const getPrice = useMemo(() => {
 		const size = props.sizes.find((size) => size.name === currentSize);
 		return props.basePrice + size.additionalPrice;
-	};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentSize]);
 
-	const addToCart = (e) => {
+	const addToCart = useCallback((e) => {
 		e.preventDefault();
-		console.log(`Summary \n==========\n${props.title}\n${getPrice()}\nSize: ${currentSize}\nColor: ${currentColor}`);
-	};
+		console.log(`Summary \n==========\n${props.title}\n${getPrice}\nSize: ${currentSize}\nColor: ${currentColor}`);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<article className={styles.product}>
@@ -23,7 +25,7 @@ const Product = (props) => {
 			<div>
 				<header>
 					<h2 className={styles.name}>{props.title}</h2>
-					<span className={styles.price}>Price: {getPrice()}$</span>
+					<span className={styles.price}>Price: {getPrice}$</span>
 				</header>
 				<ProductForm
 					sizes={props.sizes}
